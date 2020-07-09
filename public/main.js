1665eef6d7a2ce398ced9c2dfccd28c9c1bddcd8
Ryuta Nakayama <217c0314@s.obirin.ac.jp>
2020-07-09 18:11:04 +0900

    送信者表示と吹き出し

--- public/main.js
+++ public/main.js
@@ -1,15 +1,32 @@
 const socketio = io();
-    const form = document.getElementById("form");
-    const input = document.getElementById("msg");
-    const chats = document.getElementById("chats");
+const form = document.getElementById("form");
+const input = document.getElementById("msg");
+const chats = document.getElementById("chats");
 
-    form.addEventListener('submit', function(event){
-      socketio.emit('message', input.value);
-      input.value='';
-      event.preventDefault();
-    })
-    socketio.on('message',function(msg){
-      const li = document.createElement("li");
-      li.append(msg);
-      chats.append(li);
-    });
+const nameform = document.getElementById("nameform");
+const name = document.getElementById("name");
+
+let username='';
+nameform.addEventListener('submit', function(event){
+  username = name.value;
+  event.preventDefault();
+  nameform.style.display ="none";
+  form.style.display ="block";
+})
+
+form.addEventListener('submit', function(event){
+  const msg = JSON.stringify({msg: input.value, name: username})
+  socketio.emit('message', msg);
+  input.value='';
+  event.preventDefault();
+})
+socketio.on('message',function(msg){
+  const obj = JSON.parse(msg);
+
+  const dt = document.createElement("dt");
+  const dd = document.createElement("dd");
+  dt.append(obj.name);
+  chats.append(dt);
+  dd.append(obj.msg);
+  chats.append(dd);
+});
